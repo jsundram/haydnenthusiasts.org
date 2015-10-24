d435a41e8f72339474b61b8847b8078741f27367
Andrew Wong <wongavision@gmail.com>
2015-10-24 02:17:21 +0200

    added links to performers

--- pages/enthusiasts.py
+++ pages/enthusiasts.py
@@ -5,6 +5,7 @@ def get_context():
     performers = json.load(open('./static/data.json', 'r'))['performers']
     for performer in performers:
         performer['photo'] = u'/assets/people/' + performer['photo']
+        performer['link'] = performer['name'].replace(' ', '_')
 
     return {
         'performers': performers
