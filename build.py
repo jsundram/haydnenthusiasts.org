5d7bc778f4ee035bf16b1db99d31706d7a6fd4be
jsundram <jsundram@gmail.com>
2015-11-01 11:37:05 -0800

    use functions

--- build.py
+++ build.py
@@ -16,7 +16,6 @@ from subprocess import call
 import os
 import pages
 
-############################
 
 SITE_DEPLOY = './site-deploy'
 STATIC_ROOT = './static/'
@@ -29,41 +28,48 @@ SITE = {
     'haydn_seek.html': pages.haydn_seek
 }
 
-############################
 
-
-if __name__ == "__main__":
-    # first blow everything away
-    print 'setting up', SITE_DEPLOY
+def stage():
+    # First, blow everything away.
+    print('setting up', SITE_DEPLOY)
     if os.path.exists(SITE_DEPLOY):
         call('rm -r ' + SITE_DEPLOY + '/*', shell=True)
     else:
         os.makedirs(SITE_DEPLOY)
 
-    # copy a fresh batch of static files
-    print 'copying static files'
+    # Copy a fresh batch of static files.
+    print('copying static files')
     call(['cp', '-r', STATIC_ROOT, SITE_DEPLOY])
 
-    # render the templates
+
+def render():
+    """Render our templates."""
     loader = FileSystemLoader(TEMPLATE_ROOT)
     env = Environment(loader=loader)
 
-    for tmpl_name, context in SITE.iteritems():
-        print 'building', tmpl_name
-        tmpl = env.get_template(tmpl_name)
+    for name, context in SITE.iteritems():
+        print('building', name)
+        template = env.get_template(name)
+
         context = context or {}
         if hasattr(context, '__call__'):
             context = context()
-        # inject the name of the template in the context
-        context['template_name'] = tmpl_name
-        if context:
-            out = tmpl.render(**context)
-        else:
-            out = tmpl.render()
+        # Inject template name into the context
+        context['template_name'] = name
 
-        out_file = os.path.join(SITE_DEPLOY, tmpl_name)
+        out = template.render(**context)
+
+        out_file = os.path.join(SITE_DEPLOY, name)
 
         with open(out_file, 'w') as f:
             f.write(out.encode('utf-8'))
 
+
+def main():
+    stage()
+    render()
     print 'finished'
+
+
+if __name__ == "__main__":
+    main()
