075d3116ba756c7d24e75e0d45f3793419df16f6
Jason Sundram <jsundram@gmail.com>
2025-01-09 13:40:28 -0500

    don't need a full sort, max is enough (and cleaner)

--- pages/haydn_card.py
+++ pages/haydn_card.py
@@ -2,7 +2,6 @@
 Context generator for the Haydn Info Card
 """
 
-import os
 from pathlib import Path
 from datetime import datetime
 
@@ -10,14 +9,13 @@ from datetime import datetime
 def get_context():
     card_root = Path('./external/haydn-info-card/output/')
 
-    # Get sorted PDF files based on dates in filenames
-    # filenames look like 'Haydn Info Card - Sundram - 2024-04-14.pdf'
-    latest_card = sorted(
+    # Filenames look like 'Haydn Info Card - Sundram - 2024-04-14.pdf'
+    # Parse the date part out of the filename, and get the latest one.
+    latest_card = max(
         card_root.glob('*.pdf'),
         key=lambda f: datetime.strptime(f.stem.split(' - ')[-1], '%Y-%m-%d'),
-        reverse=True
-    )[0]
-    # if this barfs on the line above because the list is empty, you may need
+    )
+    # If this barfs on the line above because the list is empty, you may need
     # to `git submodule update --init --recursive` to pull in haydn-info-card
 
     return {'card': latest_card.name}
